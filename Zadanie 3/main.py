ac0ab72ec45c58e4e0cefd73225db636e8aff533
230043 Michalina Wysocka <230043@edu.p.lodz.pl>
2023-06-07 23:01:41 +0200

    [Added] working graph, only more conclusions left

--- Zadanie 3/main.py
+++ Zadanie 3/main.py
@@ -1,18 +1,16 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.tree import DecisionTreeClassifier, plot_tree, export_graphviz
 from sklearn.metrics import confusion_matrix, classification_report
 import matplotlib.pyplot as plt
 import seaborn as sns
+import graphviz
 
 # Wczytujemy zbiór danych irysów
 iris = load_iris()
 
 # Dzielimy zbiór irysów na część treningową (80%) i testową (20%)
 X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2)
-# X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
-# (jeśli na końcu parametrów dodamy random_state, zapewni to powtarzalność tego samego podziału danych)
-# (bez tego są dzielone losowo za każdym razem i jest większa różnorodność wyników)
 
 # Wyświetlamy diagram drzewa decyzyjnego
 plt.figure()
@@ -20,9 +18,9 @@ plt.figure()
 # fit() dopasowuje model do danych treningowych, ucząc go,
 # jak przewidywać etykiety klas na podstawie dostarczonych cech
 clf = DecisionTreeClassifier().fit(X_train, y_train)
-plot_tree(clf, filled=True)
-plt.title("Drzewo decyzyjne trenowane na zbiorze irysów")
-plt.show()
+# plot_tree(clf, filled=True)
+# plt.title("Drzewo decyzyjne trenowane na zbiorze irysów")
+# plt.show()
 
 # Macierz pomyłek w formie terminalowej...
 predicted_labels = clf.predict(X_test)
@@ -44,7 +42,10 @@ print("\nRaport klasyfikacji:")
 print(class_report)
 # support = ile próbek należy do danej klasy w zbiorze testowym
 
-
-
-
-
+dot_data = export_graphviz(clf, out_file=None,
+                           feature_names=iris.feature_names,
+                           class_names=iris.target_names,
+                           filled=True, rounded=True,
+                           special_characters=True)
+graph = graphviz.Source(dot_data)
+graph.render("iris")
